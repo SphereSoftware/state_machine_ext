d50eb4f185e25923607a6e22b906b17181fdf6dd
dlandberg <DLandberg@sphereconsultinginc.com>
2010-11-26 15:58:09 +0200

    init

---
+++ lib/ex_machine.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'state_machine'
+
+module ExStateMachine
+
+  def self.included(base)
+    base.class_eval do
+      alias_method :define_event_helpers_original, :define_event_helpers
+
+      def define_event_helpers
+        define_event_helpers_original
+
+        next_transitions = []
+        define_instance_method(attribute(:all_transitions)) do |machine, object, *args|
+
+          transitions = machine.events.transitions_for(object, *args)
+
+          transitions.each do |transition|
+            unless transition.to == object.state
+              next_transition = object.state_all_transitions(:from => transition.to_name)
+
+              next_transitions << next_transition unless next_transition.empty?
+              next_transitions << transition
+            end
+          end
+
+          res = []
+          next_transitions.each do |trans|
+            res << trans.to_name unless trans.is_a?(Array)
+          end
+
+          res.uniq
+        end
+      end
+    end
+  end
+
+end
+
+class StateMachine::Machine
+  include ExStateMachine
+end
