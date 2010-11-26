e3bf9ad0010b0033e063c7e9f7572b83dc1f41f1
dlandberg <DLandberg@sphereconsultinginc.com>
2010-11-26 16:59:22 +0200

    implement all transitions from state

--- lib/ex_machine.rb
+++ lib/ex_machine.rb
@@ -12,12 +12,13 @@ module ExStateMachine
 
         next_transitions = []
         define_instance_method(attribute(:all_transitions)) do |machine, object, *args|
-
+          *args = {:from => args[0]} if !(args.empty?)
+      
           transitions = machine.events.transitions_for(object, *args)
 
           transitions.each do |transition|
             unless transition.to == object.state
-              next_transition = object.state_all_transitions(:from => transition.to_name)
+              next_transition = object.state_all_transitions(transition.to_name)
 
               next_transitions << next_transition unless next_transition.empty?
               next_transitions << transition
